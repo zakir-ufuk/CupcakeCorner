11eca490de32738521b088d88055ef34d825e8dd
Zakir Ufuk Sahiner <z.ufuk.sahiner@gmail.com>
2024-03-18 15:36:13 +0100

    Initial commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Zakir Ufuk Sahiner on 18.03.24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
